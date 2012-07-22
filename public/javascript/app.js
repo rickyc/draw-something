43431ad11b0cd87f1cb5a7c635a12c88065b3edb
Ricky Cheng <rickyc.us@gmail.com>
2012-07-22 13:51:12 -0400

    initial commit, nodejs app for displaying draw something drawings

---
+++ public/javascript/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('li a').lightBox({
+    imageLoading: '/images/lightbox-ico-loading.gif',
+	imageBtnClose: '/images/lightbox-btn-close.gif',
+	imageBtnPrev: '/images/lightbox-btn-prev.gif',
+	imageBtnNext: '/images/lightbox-btn-next.gif'
+  });
+});
